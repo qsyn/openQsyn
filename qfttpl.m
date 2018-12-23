1914dd2e5113d46f5b9b600005b5c4f65cb1a9ec
bzj8vr <bzj8vr@192.168.122.1>
2018-12-23 14:31:57 +0200

    first commit

---
+++ qfttpl.m
@@ -0,0 +1,15 @@
+classdef qfttpl
+    %QFTTPL Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        model   % parametric model
+    end
+    
+    methods
+        function tpl = qfttpl(expression)
+            
+    end
+    
+end
+
