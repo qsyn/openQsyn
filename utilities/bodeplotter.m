14f2cd852da742959c56d6186071d8a0c6538e08
GalBarkai <53426284+GalBarkai@users.noreply.github.com>
2020-05-05 16:23:04 +0300

    Fixed the overwrite problem
    Fixed the overwrite issue, a byproduct is that the colors in "col" are shifted by 1. Not a major issue, still thinking about how to solve it.

--- utilities/bodeplotter.m
+++ utilities/bodeplotter.m
@@ -21,7 +21,8 @@ if strcmp(opt,'magphase')
 end
 
 if ~strcmp(opt,'phase')
-    set(gca, 'ColorOrder', col, 'NextPlot', 'replacechildren');
+    hold on
+    set(gca, 'ColorOrder', col, 'NextPlot', 'add','XScale','log');
     semilogx(w,imag(tpl),'linewidth',linespec.width,'linestyle',linespec.style);  
     %if strcmp(CASE,'all') || strcmp(CASE,'nom')
     %    [w_nom,t_nom]=gettpl(tplf,'nom');
@@ -38,7 +39,8 @@ if strcmp(opt,'magphase')
 end
 
 if ~strcmp(opt,'mag')
-    set(gca, 'ColorOrder', col, 'NextPlot', 'replacechildren');
+    hold on
+    set(gca, 'ColorOrder', col, 'NextPlot', 'add','XScale','log');
     semilogx(w,real(tpl),'linewidth',linespec.width,'linestyle',linespec.style);
     %if strcmp(CASE,'all') || strcmp(CASE,'nom')
     %    phase=real(t_nom);
