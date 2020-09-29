a1dacbf26f495189e81a5b83f1b5054ce47f0979
unknown <GalBarkai@gmail.com>
2020-09-29 16:22:10 +0300

    Added basic methods for time-domain analysis with headers

--- qsys.m
+++ qsys.m
@@ -242,6 +242,213 @@ classdef qsys
             %FEEDBACK connects qplant or qsys objects by negative feedback
             sys = qsys({A,B},'1/(1+B{1}*B{2})');
         end
+        function varargout = step(varargin)
+            % STEP is used to calculate and plot the step response of a
+            % QSYS object for a given set of parameters and a time vector.
+            %  Usage: 
+            %           y = STEP(obj,pars,t)   computes the m step
+            %           responses of obj for the m different cases of the n
+            %           uncertain parameters given by the nXm matrix
+            %           "pars", for the time vector t. 
+            %
+            %           STEP(obj,pars,t)        plots the responses
+            %
+            %  Inputs:
+            %           obj     a QSYS object representing an
+            %                   interconnection of systems.
+            %
+            %           pars    an array with n rows, each corresponds to
+            %                   a QPAR object, and m colums each
+            %                   corresponds to a test case to be simulated.
+            %                   For example if the uncertain plant inside
+            %                   is denoted P with P.pars.name a,k,wn,z,
+            %                   then pars would have 4 rows. If pars is
+            %                   empty, the nominal values will be
+            %                   simulated.
+            %
+            %           t       a time vector for the simulation, i.e.
+            %                   t=0:0.1:30. If no time vector is supplied,
+            %                   the default is 0:0.1:10
+            defaultT=0:0.1:10;
+            nomPars=1;
+            switch nargin
+                case 1
+                    obj=varargin{1};
+                    assert(isa(obj,'qsys'),'Must input a qsys object!')
+                    t=defaultT;
+                    pars=nomPars;
+                case 2
+                    obj=varargin{1};
+                    assert(isa(obj,'qsys'),'Must input a qsys object!')
+                    if (isrow(varargin{2}))
+                        t=varargin{2};
+                        pars=nomPars;
+                    else
+                        pars=varargin{2};
+                        t=defaultT;
+                    end
+                case 3
+                    obj=varargin{1};
+                    pars=varargin{2};
+                    t=varargin{3};
+            end
+            [~,nCases]=size(pars);
+            y=zeros(length(t),nCases);
+            for ii=1:nCases
+                [sysNum,sysDen]=CoeffExtract(obj,pars(:,ii));
+                [a,b,c,d] = qplant.Local_tf2ss(sysNum,sysDen);
+                x0=zeros(length(a),1);
+                [t x] = ode45(@(t,x) odeFun(t,x,a,b), t, x0);
+                y(:,ii) = c*x'+d;
+            end
+            col=lines(nCases);
+            linespec = struct('width',1,'style','-');
+            figure
+                hold on
+                set(gca, 'ColorOrder', col, 'NextPlot', 'add')
+                plot(t,y,'linewidth',linespec.width,'linestyle',linespec.style);
+                xlabel('Time [s]');
+                ylabel('Amplitude');
+                title('Step Response')
+                axis tight
+                hold off
+                if nargout
+                    varargout{1}=y;
+                end
+            function dxdt = odeFun(t,x,A,B)
+            u = 1; % for step input
+            dxdt = A*x+B; % simply write the equation
+            end 
+            end
+        function [sysNum,sysDen]=CoeffExtract(obj,pars)
+            % COEFFEXTRACT is used to extract the numerator/denominator
+            % coefficient vectors of the form
+            %               NUM(s)
+            %       H(s) = -------
+            %               DEN(s)
+            % from a QSYS object for time domain simulations.
+            %  Usage: 
+            %           [SYSNUM,SYSDEN] = COEFFEXTRACT(obj,pars)
+            %           recursively finds the basic QSYS interconnection
+            %           involving one QPLANT and antoher system and calls
+            %           for REDUCEDSOLVER which solves the base case.
+            %
+            %  Inputs:
+            %           obj     a qsys object representing an
+            %                   interconnection of systems.
+            %
+            %           pars    an array with n rows, each corresponds to
+            %                   a QPAR object, and m colums each
+            %                   corresponds to a test case to be simulated.
+            %                   For example if the uncertain plant inside
+            %                   is denoted P with P.pars.name a,k,wn,z,
+            %                   then pars would have 4 rows. If pars is
+            %                   empty, the nominal values will be
+            %                   simulated. 
+            connection=obj.connections;
+            if isa(obj.blocks{1},'qsys')
+                [numBlk1,denBlk1]=CoeffExtract(obj.blocks{1},pars);
+            end
+            if isa(obj.blocks{2},'qsys')
+                [numBlk2,denBlk2]=CoeffExtract(obj.blocks{2},pars);
+            end
+
+            Bool1=exist('numBlk1','var');
+            Bool2=exist('numBlk2','var');
+            if (Bool1&&~Bool2)       
+                Blk1=qplant(numBlk1,denBlk1); %%% FINISH THIS!
+                obj=qsys({Blk1,obj.blocks{2}},connection);
+            end
+            if (Bool2&&~Bool1)       
+                Blk2=qplant(numBlk2,denBlk2); 
+                obj=qsys({obj.blocks{1},Blk2},connection);
+            end
+            if (Bool2&&Bool1)
+                indInt=find(denBlk2,1,'last');
+                indDif=find(numBlk2,1,'last');
+                gain=numBlk2(indDif)/denBlk2(indInt);
+
+                Blk2=qctrl(roots(numBlk2),roots(denBlk2),gain);
+                Blk1=qplant(numBlk1,denBlk1);
+                obj=qsys({Blk1,Blk2},connection);
+            end
+
+            [sysNum,sysDen]=ReducedSolver(obj,pars);
+        end
+        function [sysNum,sysDen]=ReducedSolver(obj,pars)
+            % REDUCEDSOLVER solves the base-case of a QSYS object involving
+            % a single QPLANT object and another QCTRL/TF/ZPK/DOUBLE
+            % object. It outputs the numerator/denominator data of the form
+            %               NUM(s)
+            %       H(s) = -------
+            %               DEN(s)
+            % for the interconnected object.            
+            %  Usage: 
+            %           [SYSNUM,SYSDEN] = REDUCEDSOLVER(obj,pars)
+            %           finds the basic QSYS interconnection
+            %           involving one QPLANT and antoher system. It solves
+            %           the base case of COEFFEXTRACT
+            %  Inputs:
+            %           obj     a QSYS object representing an
+            %                   interconnection of systems.
+            %
+            %           pars    an array with n rows, each corresponds to
+            %                   a QPAR object, and m colums each
+            %                   corresponds to a test case to be simulated.
+            %                   For example if the uncertain plant inside
+            %                   is denoted P with P.pars.name a,k,wn,z,
+            %                   then pars would have 4 rows. If pars is
+            %                   empty, the nominal values will be
+            %                   simulated. 
+            assert((isa(obj,'qsys') && isa(pars,'numeric')),'Input must be (qsys,numeric)')
+
+            feedCon='1/(1+B{1}*B{2})';
+            serCon='B{1}*B{2}';
+            for k=1:length(obj.blocks)
+                blk = obj.blocks{k};
+                switch class(blk)
+                    case 'zpk'
+                        num=blk.K*poly(blk.Z{:}); %maybe need to fix orders
+                        den=poly(blk.P{:});
+                    case 'qctrl'
+                          [num,den] = tfdata(blk);
+                    case 'tf'
+                        num=blk.Numerator{:};
+                        den=blk.Denominator{:};
+                    case 'double'
+                        num=blk;
+                        den=1;
+                    case 'qplant'
+                        Plant=blk;
+                end
+            end
+            if ~isempty(Plant.pars)
+                if isscalar(pars)
+                   numP=cases(Plant.num);
+                   denP=cases(Plant.den);
+                else
+                    [numpars,denpars]=Prase_params(Plant,pars);
+                    numP=cases(Plant.num,numpars);
+                    denP=cases(Plant.den,denpars);
+                end
+            else
+                numP=Plant.num;
+                denP=Plant.den;
+            end
+            numProd=conv(numP,num);
+            denProd=conv(denP,den);
+            padSize=length(denProd)-length(numProd);
+            numProd=[zeros(1,padSize),numProd];
+
+            switch obj.connections
+                case feedCon
+                    sysNum=denProd;
+                    sysDen=denProd+numProd;
+                case serCon
+                    sysNum=numProd;
+                    sysDen=denProd;
+            end
+        end
         
     end
 end
